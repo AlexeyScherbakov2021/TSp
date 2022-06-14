afc6a96a483efac7ae41cc7968f2a47d164f0ac2
AlexeyScherbakov2021 <oppose2@yandex.ru>
2022-06-14 11:46:35 +0400

    Вложенный список отделов

--- TSp/ClientApp/src/components/Otdel.js
+++ TSp/ClientApp/src/components/Otdel.js
@@ -1,5 +1,66 @@
 ﻿import React, { Component } from 'react';
 
+var listOtdel = [
+    {
+        id: 1,
+        name: "Аппарат управления"
+    },
+    {
+        id: 2,
+        name: "Группа системного администрирования"
+    },
+    {
+        id: 3,
+        name: "Инженерный отдел",
+        child: [
+            {
+                id: 3.1,
+                name: "Участок Автоматизированных Систем Управления"
+            },
+            {
+                id: 3.2,
+                name: "Участок сопровождения"
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: "Информационно-технический отдел"
+    },
+    {
+        id: 5,
+        name: "Испытательный центр"
+    },
+    {
+        id: 6,
+        name: "Конструкторский отдел"
+    },
+    {
+        id: 7,
+        name: "Отдел бухгалтерского учета"
+    }
+]
+
+
+const Otdels = ({ data }) => {
+    const { id, name, child } = data;
+
+    return (
+        <>
+        <li className="list-group-item">
+            <a href={"#"} type="button">{name }</a>
+        </li>
+        <ul>
+        {
+            child &&
+                child.map((i, n) => <Otdels data={i} key={n} />)
+            }
+            </ul>
+            </>
+        );
+};
+
+
 export class Otdel extends Component {
     static displayName = Otdel.name;
 
@@ -17,6 +78,15 @@ export class Otdel extends Component {
         };
 
 
+        function handleData(arr) {
+
+            // возвращаем полученный массив компонентов
+            return [...arr.map((i, n) => <Otdels data={i} key={n} />)];
+
+        }
+
+        let showHTMLData = handleData(listOtdel);
+
         return (
 
             <aside className="col-3 d-none d-md-block col-lg-4" style={asideStyle}>
@@ -24,18 +94,8 @@ export class Otdel extends Component {
                     <h5>Весь список</h5>
                 </div>
                 <ul className="list-group border rounded overflow-auto" style={ulStyle}>
-                    <li className="list-group-item">
-                        <a href={"#"} type="button">Отдел информационных технологий</a>
-                    </li>
-                    <li className="list-group-item">
-                        <a href={"#"} type="button">Отдел маркетинга</a>
-                    </li>
-                    <li className="list-group-item">
-                        <a href={"#"} type="button">Отдел менеджмента качества</a>
-                    </li>
-                    <li className="list-group-item">
-                        <a href={"#"} type="button">Инженерный отдел</a>
-                    </li>
+                    {handleData(listOtdel)}
+
                 </ul>
             </aside>
             );
